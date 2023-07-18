1be23e06a8b7f31e140184fafac4f422dab08919
Suuuuuqi <139861678+Suuuuuqi@users.noreply.github.com>
2023-07-18 20:52:56 +0800

    stuScholarship_examine.py

--- pages/stuScholarship_examine.py
+++ pages/stuScholarship_examine.py
@@ -45,36 +45,36 @@ def Examine():
 choice1 = st.sidebar.selectbox('   请下拉选择框', ["待办事项","审批详情","审批材料"])
 if choice1=='待办事项':
     #显示待办事项
-    image=Image.open(r'.\pictures\a1.png')
+    image=Image.open(r'./pictures/a1.png')
     st.image(image)
 elif choice1 == '审批详情':
      #显示个人记录
-    image=Image.open(r'.\pictures\a2.png')
+    image=Image.open(r'./pictures/a2.png')
     st.image(image)
 elif choice1 == '审批材料':
      #显示个人记录
-    image1=Image.open(r'.\pictures\a3.png')
+    image1=Image.open(r'./pictures/a3.png')
     st.image(image1)
     #如果勾选就显示数据框
     c51,c52,c53,c54,c55=st.columns(5)
     if c51.checkbox('学习成绩'):
-        image2=Image.open(r'.\pictures\a3.1.png')
+        image2=Image.open(r'./pictures/a3.1.png')
         st.image(image2)
         Examine()
     if c52.checkbox('科研成果'):
-        image2=Image.open(r'.\pictures\a3.2.png')
+        image2=Image.open(r'./pictures/a3.2.png')
         st.image(image2)
         Examine()
     if c53.checkbox('科创竞赛'):
-        image2=Image.open(r'.\pictures\a3.3.png')
+        image2=Image.open(r'./pictures/a3.3.png')
         st.image(image2)
         Examine()
     if c54.checkbox('民主评议'):
-        image2=Image.open(r'.\pictures\a3.4.png')
+        image2=Image.open(r'./pictures/a3.4.png')
         st.image(image2)
         Examine()
     if c55.checkbox('社会活动'):
-        image2=Image.open(r'.\pictures\a3.5.png')
+        image2=Image.open(r'./pictures/a3.5.png')
         st.image(image2)
         Examine()
 
